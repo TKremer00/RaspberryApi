356043ba3a6fbfe0f3bae71153ef1f660650286a
TKremer00 <tim.kremer1@student.graafschapcollege.nl>
2019-10-25 15:30:42 +0200

    minor changes

--- src/main/java/sensor/TouchSensor.java
+++ src/main/java/sensor/TouchSensor.java
@@ -8,7 +8,6 @@ public class TouchSensor {
     private final static GpioController gpio = GpioFactory.getInstance();
     private final static GpioPinDigitalInput sensor = gpio.provisionDigitalInputPin(Touch.getPin(), PinPullResistance.PULL_DOWN);
 
-
     public static GpioPinDigitalInput getTouchSensor() {
         return sensor;
     }
